605f802e3cefab66ce59cb9ad10764fbd381bb87
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-08-20 23:08:22 +0600

    chore: import animate plugin insted of required

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-import type { Config } from "tailwindcss"
+import animatePlugin from "tailwindcss-animate"
 
+import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
@@ -74,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animatePlugin],
 } satisfies Config
 
 export default config
